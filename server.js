c0871811d5afe95901cbb9a6db26c609b6fe73ff
sabelogumede <iamsabelogumede@gmail.com>
2019-08-03 10:11:52 +0200

    added get route and jquery function to load our server data

--- server.js
+++ server.js
@@ -3,6 +3,24 @@ const app = express()
 
 app.use(express.static(__dirname))
 
+//messages list
+const messages = [
+    {name: 'Sabelo', message: 'Hi'},
+    {name: 'Fezeka', message: 'Hello'}
+]
+
+// app get route/url
+app.get('/messages', (req, res) =>{
+    res.send(messages)
+})
+
+
+
+
+
+
+
+
 const server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
